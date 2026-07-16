6cc448f82c80edc93a73da9c721144afdb12ceb2
hagarmohamed25 <hagarmohamed0025@gmail.com>
2026-07-16 10:08:27 +0300

    Navbar of home

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import json
 import os
 
 app = Flask(__name__)
+app.secret_key = "mysecretkeyishidden"  
 
 DATABASE = "Database/users.json"
 
 
-# to read users
 def load_users():
     if not os.path.exists(DATABASE):
         return []
@@ -16,15 +16,38 @@ def load_users():
         return json.load(file)
 
 
-# to save users
+
 def save_users(users):
     with open(DATABASE, "w") as file:
         json.dump(users, file, indent=4)
 
 
+
 @app.route("/")
+def home_redirect():
+    return redirect(url_for("home"))
+
+
+
+
+
+@app.route("/home")
 def home():
-    return redirect(url_for("login"))
+    return render_template("home.html")
+
+
+
+@app.route("/admin")
+def admin_dashboard():
+
+    if "username" not in session:
+        return redirect(url_for("login"))
+
+    if session.get("role") != "admin":
+        return redirect(url_for("home"))
+
+    return render_template("admin.html")
+
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -38,7 +61,7 @@ def register():
 
         users = load_users()
 
-        # Check if the email exists
+        # Check if email already exists
         for user in users:
             if user["email"] == email:
                 return "Email already exists!"
@@ -47,12 +70,15 @@ def register():
             "username": username,
             "email": email,
             "password": password,
-            "role": "customer"
+            "role": "customer"      # Default role
         })
+
         save_users(users)
 
         return redirect(url_for("login"))
+
     return render_template("register.html")
+
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -65,15 +91,65 @@ def login():
 
         users = load_users()
 
-        #to check if the email and password exist in database
         for user in users:
-            print("Comparing with:", user["email"])
 
             if user["email"] == email and user["password"] == password:
-                return f"Welcome {user['username']}!"
+
+                # Save user information
+                session["username"] = user["username"]
+                session["email"] = user["email"]
+                session["role"] = user["role"]
+
+                # Redirect according to role
+                if user["role"] == "admin":
+                    return redirect(url_for("admin_dashboard"))
+
+                return redirect(url_for("home"))
 
         return "Invalid email or password."
+
     return render_template("login.html")
+
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("home"))
+
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+
+@app.route("/policies")
+def policies():
+    return render_template("policies.html")
+
+
+
+@app.route("/category/<name>")
+def category(name):
+    return render_template("category.html", category=name)
+
+
+
+@app.route("/cart")
+def cart():
+
+    if "username" not in session:
+        return redirect(url_for("login"))
+
+    return render_template("cart.html")
+
+
+
+@app.route("/search")
+def search():
+    return render_template("search.html")
 
 
 if __name__ == "__main__":
