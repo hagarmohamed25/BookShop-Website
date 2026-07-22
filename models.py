d61a2d9ba9bf3fb0f613f9e991a4d5edf2c7da73
hagarmohamed25 <hagarmohamed0025@gmail.com>
2026-07-22 10:59:32 +0300

    move the class to the models file

--- models.py
+++ models.py
@@ -1,32 +1,44 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 import json
 import os
+import uuid
+from datetime import datetime
 
-BOOKS_FILE = "database/books.json"
-USERS_FILE = "database/users.json"
+class Book_Shop:
+    USERS_FILE = None
+    BOOKS_FILE = None
 
+    def __init__(self, USERS_FILE, BOOKS_FILE):
+        self.USERS_FILE = USERS_FILE
+        self.BOOKS_FILE = BOOKS_FILE
 
-def get_books():
-    if not os.path.exists(BOOKS_FILE):
-        return {"categories": []}
+    def load_users(self):
+        """Load users from JSON file"""
+        if not os.path.exists(self.USERS_FILE):
+            return []
+        try:
+            with open(self.USERS_FILE, "r") as f:
+                return json.load(f)
+        except:
+            return []
 
-    with open(BOOKS_FILE, "r", encoding="utf-8") as file:
-        return json.load(file)
+    def save_users(self,users):
+        """Save users to JSON file"""
+        with open(self.USERS_FILE, "w") as f:
+            json.dump(users, f, indent=4)
 
+    def load_books(self):
+        """Load books from JSON file"""
+        if not os.path.exists(self.BOOKS_FILE):
+            return []
+        try:
+            with open(self.BOOKS_FILE, "r") as f:
+                data = json.load(f)
+                return data.get("categories", [])
+        except:
+            return []
 
-def save_books(data):
-    with open(BOOKS_FILE, "w", encoding="utf-8") as file:
-        json.dump(data, file, indent=4)
-
-
-
-def get_users():
-    if not os.path.exists(USERS_FILE):
-        return []
-
-    with open(USERS_FILE, "r", encoding="utf-8") as file:
-        return json.load(file)
-
-
-def save_users(users):
-    with open(USERS_FILE, "w", encoding="utf-8") as file:
-        json.dump(users, file, indent=4)
+    def save_books(self,categories):
+        """Save books to JSON file"""
+        with open(self.BOOKS_FILE, "w") as f:
+            json.dump({"categories": categories}, f, indent=4)
